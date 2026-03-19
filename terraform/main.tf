9778a0a390f95c788bc50095bc3b5fcb8096e494
Nonit <choudhary.nonit4@gmail.com>
2026-03-20 00:09:22 +0530

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,42 +10,18 @@ resource "aws_lambda_function" "lambda" {
 
   image_uri = var.image_uri
 
-  role = try(data.aws_iam_role.lambda_role_existing.arn, aws_iam_role.lambda_role[0].arn)
+  role = data.aws_iam_role.lambda_role_existing.arn
 
   lifecycle {
     create_before_destroy = true
-    replace_triggered_by  = [aws_iam_role.lambda_role]
   }
 
   depends_on = [aws_iam_role_policy_attachment.lambda_basic]
 }
 
-resource "aws_iam_role" "lambda_role" {
-  count = try(data.aws_iam_role.lambda_role_existing.id, null) != null ? 0 : 1
-
-  name = "lambda-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
 
-  role       = try(data.aws_iam_role.lambda_role_existing.name, aws_iam_role.lambda_role[0].name)
+  role       = data.aws_iam_role.lambda_role_existing.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 
   lifecycle {
