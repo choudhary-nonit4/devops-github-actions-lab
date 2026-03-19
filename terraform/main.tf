7d4e280f2cfc4386c2937e86020b68ea8385a631
Nonit <choudhary.nonit4@gmail.com>
2026-03-20 00:42:22 +0530

    RDS instance creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,67 @@
-data "aws_iam_role" "lambda_role_existing" {
-  name = "lambda-execution-role"
+provider "aws" {
+  region = var.region
 }
 
-resource "aws_lambda_function" "lambda" {
-
-  function_name = "devops-ecr-lambda"
-
-  package_type = "Image"
-
-  image_uri = var.image_uri
-
-  role = try(data.aws_iam_role.lambda_role_existing.arn, aws_iam_role.lambda_role[0].arn)
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_iam_role" "lambda_role" {
-  count = try(data.aws_iam_role.lambda_role_existing.id, null) != null ? 0 : 1
-
-  name = "lambda-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
+# Get subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
+# Security Group
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-public-sg"
+  description = "Allow DB access"
+  vpc_id      = data.aws_vpc.default.id
 
-  role       = try(data.aws_iam_role.lambda_role_existing.name, aws_iam_role.lambda_role[0].name)
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ingress {
+    description = "Allow DB access"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Subnet group
+resource "aws_db_subnet_group" "default" {
+  name       = "rds-subnet-group"
+  subnet_ids = data.aws_subnets.default.ids
+}
+
+# RDS Instance
+resource "aws_db_instance" "rds" {
+  identifier              = "my-rds-instance"
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+
+  db_name                 = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+
+  publicly_accessible     = true
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  db_subnet_group_name    = aws_db_subnet_group.default.name
+
+  skip_final_snapshot     = true
+
+  tags = {
+    Name = "MyRDSInstance"
+  }
 }
--- terraform/varaibles.tf
+++ terraform/varaibles.tf
@@ -1,3 +1,20 @@
-variable "image_uri" {
-  type = string
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "db_name" {
+  default = "mydb"
+}
+
+variable "db_username" {
+  default = "admin"
+}
+
+variable "db_password" {
+  description = "RDS master password"
+  sensitive   = true
+}
+
+variable "allowed_ip" {
+  description = "Your IP to allow access"
 }
